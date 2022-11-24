140e88577a043dd901540381e0de4a6976186ae7
Mathias Pedrozo <mathias.pedrozo@hotmail.com>
2022-11-24 15:51:22 -0300

    first commit

---
+++ botones/botones.js
@@ -0,0 +1,9 @@
+function saludar(nombre, apellido){
+    console.log("Bienvenido " + nombre + " " + apellido);
+    alert("Bienvenido " + nombre + " " + apellido)
+
+}
+
+//saludar("Mathias", "Pedrozo")
+//saludar("Natalia", "Del Castillo")
+//saludar("Mishi", "Morita")
